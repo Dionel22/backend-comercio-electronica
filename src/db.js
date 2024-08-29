5420ab8b584c214db85ae9b6524b339ab3afe13f
Dionel22 <diondiaz293@gmail.com>
2024-08-29 11:38:18 -0300

    configuracion de db.js

--- src/db.js
+++ src/db.js
@@ -1,4 +1,7 @@
+require("dotenv").config();
 const { Sequelize } = require("sequelize");
+
+const { DB_USER, DB_PASSWORD, DB_HOST, DB_PORT, DB_DATABASE } = process.env;
 
 const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_DATABASE}`, {
   logging: false,
