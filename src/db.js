42c9f3c9ef542f5d7ab6a9219f41595cb98a9fa7
Dionel22 <diondiaz293@gmail.com>
2024-08-29 12:27:09 -0300

    configuracion de db.js

--- src/db.js
+++ src/db.js
@@ -1,11 +1,23 @@
 require("dotenv").config();
 const { Sequelize } = require("sequelize");
+const usuarioModel = require("@/Model/Usuario");
+const productoModel = require("@/Model/Producto");
+const pasarelaDePagoModel = require("@/Model/PasarelaDePago");
 
 const { DB_USER, DB_PASSWORD, DB_HOST, DB_PORT, DB_DATABASE } = process.env;
 
 const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_DATABASE}`, {
   logging: false,
 });
+
+usuarioModel(sequelize);
+productoModel(sequelize);
+pasarelaDePagoModel(sequelize);
+
+const { Usuario, Producto, PasarelaDePago } = sequelize.models;
+
+Usuario.belongsToMany(Producto, { through: PasarelaDePago});
+Producto.belongsToMany(Usuario, { through: PasarelaDePago});
 
 module.exports = {
     ...sequelize.models,
