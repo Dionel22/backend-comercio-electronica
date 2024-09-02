dc700315324510a2d6973a3b1e8f04b660b3c174
Dionel22 <diondiaz293@gmail.com>
2024-09-02 09:13:45 -0300

    flex de router Filtro

---
+++ src/Router/RouterFiltro/routerFiltro.js
@@ -0,0 +1,8 @@
+const { Router } = require("express");
+
+const routerFiltro = Router();
+
+routerFiltro.get("", ()=>{});
+routerFiltro.post("", ()=>{});
+
+module.exports = routerFiltro;
--- src/Router/index.js
+++ src/Router/index.js
@@ -1,11 +1,12 @@
 const { Router } = require("express");
 const routerUsuario = require("@/RouterUsuario/routerUsuario");
 const routerProducto = require("@/RouterProducto/routerProducto");
+const routerFiltro = require("@/RouterFiltro/routerFiltro");
 
 const router = Router();
 
 router.use("/Usuario", routerUsuario);
 router.use("/Producto", routerProducto);
-router.use("/Filtro", ()=>{});
+router.use("/Filtro", routerFiltro);
 
 module.exports = router;
